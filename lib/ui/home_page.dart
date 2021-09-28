6b0ce4116b9f50c8aad779cb6f90874cc22e8817
QD1221 <zxcvbnm12122000@gmail.com>
2021-09-28 15:53:11 +0700

    2nd commit

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -43,16 +44,96 @@ class _HomePageState extends State<HomePage> {
                         padding: const EdgeInsets.symmetric(vertical: 16),
                         child: Container(
                           height: 200,
+                          padding: EdgeInsets.all(8),
                           decoration: BoxDecoration(
                             color: Colors.deepPurpleAccent[200],
                             borderRadius: BorderRadius.circular(24),
+                          ),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 5,
+                                child: Stack(
+                                  children: [
+                                    Positioned(
+                                      left: 0,
+                                      right: 0,
+                                      bottom: 0,
+                                      top: 0,
+                                      child: CircularPercentIndicator(
+                                        radius: 170,
+                                        lineWidth: 7,
+                                        animation: true,
+                                        percent: 0.75,
+                                        circularStrokeCap:
+                                            CircularStrokeCap.round,
+                                        progressColor: Colors.white,
+                                        center: Padding(
+                                          padding: const EdgeInsets.all(16),
+                                          child: Container(
+                                            height: double.infinity,
+                                            width: double.infinity,
+                                            decoration: BoxDecoration(
+                                              color:
+                                                  Colors.deepPurpleAccent[100],
+                                              shape: BoxShape.circle,
+                                            ),
+                                            padding: EdgeInsets.all(8),
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                color: Colors
+                                                    .deepPurpleAccent[200],
+                                                shape: BoxShape.circle,
+                                              ),
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                children: [
+                                                  Text(
+                                                    'Remaining',
+                                                    style: TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                  Padding(
+                                                    padding:
+                                                        EdgeInsets.all(8.0),
+                                                    child: Text(
+                                                      '1,112',
+                                                      style: TextStyle(
+                                                        fontSize: 23,
+                                                        color: Colors.white,
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  Text(
+                                                    'kcal',
+                                                    style: TextStyle(
+                                                        color: Colors.white),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                flex: 4,
+                                child: Placeholder(),
+                              ),
+                            ],
                           ),
                         ),
                       ),
                       Padding(
                         padding: const EdgeInsets.symmetric(vertical: 8),
                         child: Container(
-                          height: 160,
+                          height: 140,
                           decoration: BoxDecoration(
                             color: Colors.white,
                           ),
@@ -66,16 +147,163 @@ class _HomePageState extends State<HomePage> {
                                       border: Border.all(color: Colors.grey),
                                       borderRadius: BorderRadius.circular(16),
                                     ),
+                                    padding: EdgeInsets.all(16),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              'Intaked',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20,
+                                              ),
+                                            ),
+                                            Text(
+                                              '🍕',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Container(
+                                              height: 48,
+                                              width: 8,
+                                              decoration: BoxDecoration(
+                                                color: Colors.teal[100],
+                                                borderRadius:
+                                                    BorderRadius.circular(8),
+                                              ),
+                                              child: Stack(
+                                                children: [
+                                                  Positioned(
+                                                    top: 24,
+                                                    left: 0,
+                                                    right: 0,
+                                                    bottom: 0,
+                                                    child: Container(
+                                                      decoration: BoxDecoration(
+                                                        color: Colors.teal,
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(8),
+                                                      ),
+                                                    ),
+                                                  )
+                                                ],
+                                              ),
+                                            ),
+                                            SizedBox(width: 24),
+                                            Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  '589',
+                                                  style: TextStyle(
+                                                    fontSize: 20,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                                Text('kcal'),
+                                              ],
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
                               Expanded(
                                 child: Padding(
-                                  padding: const EdgeInsets.only(left: 8),
+                                  padding: const EdgeInsets.only(right: 8),
                                   child: Container(
                                     decoration: BoxDecoration(
                                       border: Border.all(color: Colors.grey),
                                       borderRadius: BorderRadius.circular(16),
+                                    ),
+                                    padding: EdgeInsets.all(16),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              'Burned',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20,
+                                              ),
+                                            ),
+                                            Text(
+                                              '🔥',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 20,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Container(
+                                              height: 48,
+                                              width: 8,
+                                              decoration: BoxDecoration(
+                                                color: Colors.deepOrange[100],
+                                                borderRadius:
+                                                    BorderRadius.circular(8),
+                                              ),
+                                              child: Stack(
+                                                children: [
+                                                  Positioned(
+                                                    top: 24,
+                                                    left: 0,
+                                                    right: 0,
+                                                    bottom: 0,
+                                                    child: Container(
+                                                      decoration: BoxDecoration(
+                                                        color:
+                                                            Colors.deepOrange,
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(8),
+                                                      ),
+                                                    ),
+                                                  )
+                                                ],
+                                              ),
+                                            ),
+                                            SizedBox(width: 24),
+                                            Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  '738',
+                                                  style: TextStyle(
+                                                    fontSize: 20,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                                Text('kcal'),
+                                              ],
+                                            )
+                                          ],
+                                        )
+                                      ],
                                     ),
                                   ),
                                 ),
@@ -111,12 +339,8 @@ class _HomePageState extends State<HomePage> {
                             Icons.home,
                             size: 28,
                           ),
-                          SizedBox(
-                            height: 4,
-                          ),
-                          CircleAvatar(
-                            radius: 4,
-                          )
+                          SizedBox(height: 4),
+                          CircleAvatar(radius: 4)
                         ],
                       ),
                       Column(
@@ -126,11 +350,9 @@ class _HomePageState extends State<HomePage> {
                             Icons.search,
                             size: 28,
                           ),
-                          SizedBox(
-                            height: 4,
-                          ),
+                          SizedBox(height: 4),
                           // CircleAvatar(
-                          //   radius: 4,
+                          //   radius: 4
                           // )
                         ],
                       ),
@@ -151,11 +373,9 @@ class _HomePageState extends State<HomePage> {
                             Icons.bar_chart,
                             size: 28,
                           ),
-                          SizedBox(
-                            height: 4,
-                          ),
+                          SizedBox(height: 4),
                           // CircleAvatar(
-                          //   radius: 4,
+                          //   radius: 4
                           // )
                         ],
                       ),
@@ -166,11 +386,9 @@ class _HomePageState extends State<HomePage> {
                             Icons.person,
                             size: 28,
                           ),
-                          SizedBox(
-                            height: 4,
-                          ),
+                          SizedBox(height: 4),
                           // CircleAvatar(
-                          //   radius: 4,
+                          //   radius: 4
                           // )
                         ],
                       ),
